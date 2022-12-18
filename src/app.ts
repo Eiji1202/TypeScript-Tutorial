67d77d461b6e2eb0ed469db880e7214ee54169af
Eiji1202 <tsuyoku1202@gmail.com>
2022-12-18 09:53:58 +0900

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,51 @@
-interface Greetable {
-  name: string;
+//型定義
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
+//変数addの型定義
+let add: AddFn;
+
+//実装を定義
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
-  constructor(name: string) {
-    this.name = name;
+
+  constructor(name?: string) {
+    if (name) {
+      this.name = name;
+    }
   }
+
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
   }
 }
 
 //型定義
-let user1: Person;
+let user1: Greetable;
 
 //インスタンス
-user1 = new Person("Max");
+// user1 = new Person("Max");
+user1 = new Person();
 
 //関数の実行
 user1.greet("Hello I am");
